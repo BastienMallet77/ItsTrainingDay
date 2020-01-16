21200e2b0e63c5d90de706b2c5b68f7397d030a4
Batou <bastien.dd@aliceadsl.fr>
2020-01-16 11:25:11 +0100

    restController degree/ exercice

--- ItsTrainingDay-BackEnd/src/main/java/sopra/projet/ItsTrainingDay/rest/DegreeRestController.java
+++ ItsTrainingDay-BackEnd/src/main/java/sopra/projet/ItsTrainingDay/rest/DegreeRestController.java
@@ -1,17 +1,72 @@
 package sopra.projet.ItsTrainingDay.rest;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import sopra.projet.ItsTrainingDay.model.Degree;
 import sopra.projet.ItsTrainingDay.repository.DegreeRepository;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/degree")
+@CrossOrigin
 public class DegreeRestController {
 
 	@Autowired
 	private DegreeRepository degreeRepo;
+	
+	@GetMapping("")
+	public List<Degree> list()
+	{
+		List<Degree> degrees = degreeRepo.findAll();
+		
+		return degrees;
+	}
+	
+	@GetMapping("/{id}")
+	public Degree find(@PathVariable Long id) throws Exception //TODO throws ?
+	{ 
+		Optional<Degree> opt = degreeRepo.findById(id);
+
+		if (opt.isPresent()) {
+			return opt.get();
+		} else {
+			throw new Exception();
+		}
+	}
+	
+	@PostMapping("")
+	public Degree create(@RequestBody Degree degree) 
+	{
+		degree = degreeRepo.save(degree);
+
+		return degree;
+	}
+	
+	@PutMapping("/{id}")
+	public Degree update(@RequestBody Degree degree, @PathVariable Long id) 
+	{
+		degree = degreeRepo.save(degree);
+		
+		return degree;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) 
+	{
+		degreeRepo.deleteById(id);
+	}
+
 
 	
 
--- ItsTrainingDay-BackEnd/src/main/java/sopra/projet/ItsTrainingDay/rest/ExerciceRestController.java
+++ ItsTrainingDay-BackEnd/src/main/java/sopra/projet/ItsTrainingDay/rest/ExerciceRestController.java
@@ -1,18 +1,70 @@
 package sopra.projet.ItsTrainingDay.rest;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import sopra.projet.ItsTrainingDay.model.Exercice;
 import sopra.projet.ItsTrainingDay.repository.ExerciceRepository;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/exercice")
+@CrossOrigin
 public class ExerciceRestController {
 
 	@Autowired
 	private ExerciceRepository exerciceRepo;
 
+	@GetMapping("")
+	public List<Exercice> list()
+	{
+		List<Exercice> exercices = exerciceRepo.findAll();
+		return exercices;
+	}
 	
+	@GetMapping("/{id}")
+	public Exercice find(@PathVariable Long id) throws Exception 
+	{
+		Optional<Exercice> opt = exerciceRepo.findById(id);
+		
+		if(opt.isPresent()) 
+		{
+			return opt.get();
+		}else 
+		{
+			throw new Exception();
+		}
+	}
+	
+	@PostMapping("")
+	public Exercice create(@RequestBody Exercice exercice) 
+	{
+		exercice = exerciceRepo.save(exercice);
+		return exercice;
+	}
+	
+	@PutMapping("/{id}")
+	public Exercice update(@RequestBody Exercice exercice, @PathVariable Long id) 
+	{
+		exercice = exerciceRepo.save(exercice);
+		
+		return exercice;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) 
+	{
+		exerciceRepo.deleteById(id);
+	}
 
 }
