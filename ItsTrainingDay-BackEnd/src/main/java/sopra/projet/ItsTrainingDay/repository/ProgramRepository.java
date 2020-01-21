9af648c809ef69d5b0582c731da804a81113fd60
Bastien <bastien.mallet@orange.fr>
2020-01-21 14:48:16 +0100

    chercher des programmes

--- ItsTrainingDay-BackEnd/src/main/java/sopra/projet/ItsTrainingDay/repository/ProgramRepository.java
+++ ItsTrainingDay-BackEnd/src/main/java/sopra/projet/ItsTrainingDay/repository/ProgramRepository.java
@@ -1,5 +1,7 @@
 package sopra.projet.ItsTrainingDay.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +12,7 @@ public interface ProgramRepository extends JpaRepository<Program, Long> {
 	@Query("select p from Program p left join fetch p.sessions s where p.id = :id")
 	Program findByIdWithSessions(@Param("id") Long id);
 	
-	
+	@Query("select p from Program p join p.level l where l.id = :id")
+	List<Program> findAllByLevel(@Param("id")Long id);
 	
 }
